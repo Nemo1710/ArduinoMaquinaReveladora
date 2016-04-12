fbcf936d2951a437957f8e0aa0c93f488463b659
Nemo1710 <karlos1710@gmail.com>
2016-04-12 06:09:22 -0500

    funcionando con control de motor

--- final/final.ino
+++ final/final.ino
@@ -1,5 +1,13 @@
+#include <DueFlashStorage.h>
+#include <efc.h>
+#include <flash_efc.h>
+
+//objeto para el uso de memoria flash
+DueFlashStorage dueFlashStorage;
+byte motorvalores = 118;
 int motortransporte = 6;
 int motorcepillos = 7;
+float ret=22;
 int ventiladorhorno = 41;
 int reabastecimiento = 39;
 int electrovalvulas = 43;
@@ -38,6 +46,9 @@ int valmotores=0;
 int valor=0;
 String mijitrin;
 void setup() { 
+  dueFlashStorage.write(0,motorvalores);
+  dueFlashStorage.write(1,ret);
+  
   // put your setup code here, to run once:
 //pinMode(motortransporte,OUTPUT);
 pinMode(electrovalvulas,OUTPUT);
@@ -53,6 +64,7 @@ pinMode(splaca,INPUT);
 Serial.begin(9600);
 Serial1.begin(9600);
 Serial1.println("9");
+Serial.println("estamos viendo todo desde el computador");
 digitalWrite(recirculacion,LOW); 
 digitalWrite(niquelinarevelador,LOW); 
     valmotores=255;
@@ -92,27 +104,22 @@ switch(var){
         
   break;
   case 0:
-      
+
+    
+    
+
+
+
+/*      
       detener();
-      while(Serial1.available()){
-        valor=Serial1.read();
-        Serial1.println(valor);
-        
-        
-        /*if(){
+  */    
+      
+      
+      
+      
+      /*if(valor==97){
           var=1;
-          Serial.println("mijin");
-        }
-        */
-        //delay(1000);
-      }
-      
-      
-      
-      
-      if(valor==97){
-          var=1;
-       }
+       }*/
       /*if(mijitrin.length()==1){
         Serial.println(mijitrin);
         
@@ -125,6 +132,30 @@ switch(var){
       
   break;
   case 1:
+
+
+      if(Serial1.available()){
+      int orden=Serial1.read();
+      Serial.println(dueFlashStorage.read(0));
+      Serial.println(orden);
+      
+      if(orden>=48 && orden <=63){
+        //Serial.println(recepcionmotor(orden));
+        
+        dueFlashStorage.write(0,recepcionmotor(orden+3));
+        dueFlashStorage.write(1,orden-38);
+        Serial.println("Exito se recibio el dato");
+      }
+     }
+
+     ret=ret/22;
+     ret=ret*dueFlashStorage.read(1);
+     Serial.println(dueFlashStorage.read(1));
+     Serial.println(dueFlashStorage.read(0));
+     Serial.println(analogRead(sensorrevelador));
+    
+    //Serial.println(int(ret));
+    ret=22;
       mijitrin="";
       detener();
      
@@ -151,7 +182,7 @@ switch(var){
       }
       
     }*/
-    Serial.println(digitalRead(eplaca));
+    //Serial.println(digitalRead(eplaca));
     if(digitalRead(eplaca)==LOW){
       var=2;
       impresion();
@@ -164,31 +195,40 @@ switch(var){
   
   case 2:
   detener();
-     valmotores=118;
+    ret=22;
+     valmotores=dueFlashStorage.read(0);
      valcepillos=180;
     analogWrite(motortransporte,valmotores);
     analogWrite(motorcepillos,valcepillos);
     digitalWrite(reabastecimiento,HIGH);
     retardo(1);
     digitalWrite(reabastecimiento,LOW);
-    retardo(20);
+    
+    ret=ret/22;
+    ret=ret*dueFlashStorage.read(1);
+    retardo(int(ret));
+    
+    Serial.println(ret);
+    
     var=3;
      
   break;
   case 3:
     detener();
-    digitalWrite(goma,HIGH);
+    digitalWrite(niquelinahorno,HIGH);
     digitalWrite(ventiladorhorno,HIGH);
     digitalWrite(electrovalvulas,HIGH);
-    digitalWrite(niquelinahorno,HIGH);
+
+    digitalWrite(goma,HIGH);
+
     
-
-
     
     retardo(70);
     resetear();
     
   break;
+
+  
   
 }
 }
@@ -220,8 +260,8 @@ void detener(){
         reseteartotal();
       }
     }*/
-    while(Serial1.available()){
-        valor=Serial1.read();
+    while(Serial.available()){
+        valor=Serial.read();
         //Serial.println("bien");
         
         
@@ -232,10 +272,11 @@ void detener(){
         */
         //delay(1000);
       }
+      Serial.println(""+valor);
+      Serial.println(""+recepcionmotor(valor));
       
       
-      
-      
+      /*
       
       if(mijitrin.length()==1){
         Serial.println(mijitrin);
@@ -246,7 +287,7 @@ void detener(){
           
         }
         mijitrin="";
-      }
+      }*/
     
   }
 
@@ -270,7 +311,7 @@ void promedio(){
 
 
 for(int i=0;i<10;i++){
-  
+    
     valuerevelador=analogRead(sensorrevelador);
     //valuehorno=analogRead(sensorhorno);
      muestrasrevelador[i]= valuerevelador;
@@ -300,6 +341,7 @@ void retardo(int total){
     //imprimir serial
     impresion();
     detener();
+    Serial.println(dueFlashStorage.read(0));
     delay(475);
     Serial1.println(tottal);
     int h=0;
@@ -361,7 +403,7 @@ void impresion(){
     gom=0;
   }
   
-  if(valmotores==118){
+  if(valmotores==dueFlashStorage.read(0)){
     trans=1;
   }else{
     trans=0;
@@ -405,295 +447,13 @@ void impresion(){
   Serial.println(var);
 }
 
-char Decimal_to_ASCII(int entrada){
-char salida=' ';
-switch(entrada){
-case 32: 
-salida=' '; 
-break; 
-case 33: 
-salida='!'; 
-break; 
-case 34: 
-salida='"'; 
-break; 
-case 35: 
-salida='#'; 
-break; 
-case 36: 
-salida='$'; 
-break; 
-case 37: 
-salida='%'; 
-break; 
-case 38: 
-salida='&amp;'; 
-break; 
-case 39: 
-salida=' '; 
-break; 
-case 40: 
-salida='('; 
-break; 
-case 41: 
-salida=')'; 
-break; 
-case 42: 
-salida='*'; 
-break; 
-case 43: 
-salida='+'; 
-break; 
-case 44: 
-salida=','; 
-break; 
-case 45: 
-salida='-'; 
-break; 
-case 46: 
-salida='.'; 
-break; 
-case 47: 
-salida='/'; 
-break; 
-case 48: 
-salida='0'; 
-break; 
-case 49: 
-salida='1'; 
-break; 
-case 50: 
-salida='2'; 
-break; 
-case 51: 
-salida='3'; 
-break; 
-case 52: 
-salida='4'; 
-break; 
-case 53: 
-salida='5'; 
-break; 
-case 54: 
-salida='6'; 
-break; 
-case 55: 
-salida='7'; 
-break; 
-case 56: 
-salida='8'; 
-break; 
-case 57: 
-salida='9'; 
-break; 
-case 58: 
-salida=':'; 
-break; 
-case 59: 
-salida=';'; 
-break; 
-case 60: 
-salida='&lt;'; 
-break; 
-case 61: 
-salida='='; 
-break; 
-case 62: 
-salida='&gt;'; 
-break; 
-case 63: 
-salida='?'; 
-break; 
-case 64: 
-salida='@'; 
-break; 
-case 65: 
-salida='A'; 
-break; 
-case 66: 
-salida='B'; 
-break; 
-case 67: 
-salida='C'; 
-break; 
-case 68: 
-salida='D'; 
-break; 
-case 69: 
-salida='E'; 
-break; 
-case 70: 
-salida='F'; 
-break; 
-case 71: 
-salida='G'; 
-break; 
-case 72: 
-salida='H'; 
-break; 
-case 73: 
-salida='I'; 
-break; 
-case 74: 
-salida='J'; 
-break; 
-case 75: 
-salida='K'; 
-break; 
-case 76: 
-salida='L'; 
-break; 
-case 77: 
-salida='M'; 
-break; 
-case 78: 
-salida='N'; 
-break; 
-case 79: 
-salida='O'; 
-break; 
-case 80: 
-salida='P'; 
-break; 
-case 81: 
-salida='Q'; 
-break; 
-case 82: 
-salida='R'; 
-break; 
-case 83: 
-salida='S'; 
-break; 
-case 84: 
-salida='T'; 
-break; 
-case 85: 
-salida='U'; 
-break; 
-case 86: 
-salida='V'; 
-break; 
-case 87: 
-salida='W'; 
-break; 
-case 88: 
-salida='X'; 
-break; 
-case 89: 
-salida='Y'; 
-break; 
-case 90: 
-salida='Z'; 
-break; 
-case 91: 
-salida='['; 
-break; 
-case 92: 
-salida=' '; 
-break; 
-case 93: 
-salida=']'; 
-break; 
-case 94: 
-salida='^'; 
-break; 
-case 95: 
-salida='_'; 
-break; 
-case 96: 
-salida='`'; 
-break; 
-case 97: 
-salida='a'; 
-break; 
-case 98: 
-salida='b'; 
-break; 
-case 99: 
-salida='c'; 
-break; 
-case 100: 
-salida='d'; 
-break; 
-case 101: 
-salida='e'; 
-break; 
-case 102: 
-salida='f'; 
-break; 
-case 103: 
-salida='g'; 
-break; 
-case 104: 
-salida='h'; 
-break; 
-case 105: 
-salida='i'; 
-break; 
-case 106: 
-salida='j'; 
-break; 
-case 107: 
-salida='k'; 
-break; 
-case 108: 
-salida='l'; 
-break; 
-case 109: 
-salida='m'; 
-break; 
-case 110: 
-salida='n'; 
-break; 
-case 111: 
-salida='o'; 
-break; 
-case 112: 
-salida='p'; 
-break; 
-case 113: 
-salida='q'; 
-break; 
-case 114: 
-salida='r'; 
-break; 
-case 115: 
-salida='s'; 
-break; 
-case 116: 
-salida='t'; 
-break; 
-case 117: 
-salida='u'; 
-break; 
-case 118: 
-salida='v'; 
-break; 
-case 119: 
-salida='w'; 
-break; 
-case 120: 
-salida='x'; 
-break; 
-case 121: 
-salida='y'; 
-break; 
-case 122: 
-salida='z'; 
-break; 
-case 123: 
-salida='{'; 
-break; 
-case 124: 
-salida='|'; 
-break; 
-case 125: 
-salida='}'; 
-break; 
-case 126: 
-salida='~'; 
-break; 
-}
-return salida;
-}
+int recepcionmotor(int a){
+    a=a-41;
+    
+    int y=-0.1948*a*a;
+    y=y+13.032*a;
+    y=y-77.268;
+    
+    return y;
+  }
 
